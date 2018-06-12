dc79a7c4f23e7c403f48d6605b2ae02b75fa4ca8
Roel Bag-ao <rbagao@gmail.com>
2018-06-12 19:47:41 +1000

    Add project files.

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[52];
+            int randomNumber = 0;
+            Random rnd = new Random();
+            for (int i = 0; i < 52;)
+            {
+                randomNumber = rnd.Next(1, 53); // generates random number between 1 and 52
+                if (!arr.Contains(randomNumber))
+                {
+                    arr[i] = randomNumber;
+                    i++;
+                }
+            }
+        }
+    }
+}
